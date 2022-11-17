4709476cb137c414dae753ac9db35e7e975fffc9
Drian <11167300+fdrian@users.noreply.github.com>
2022-11-17 01:36:44 -0300

    Update conquer.sh

--- conquer.sh
+++ conquer.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 # Author: Drian
-# bash conquer.sh
-# Ex. bash conquer.sh BR
+# bash conquer.sh 
 
 URL="https://patoacademy.network/hit/kryptic-00001";
+PROXY="USERNAME:PASSWORD-country-$cc@la.residential.rayobyte.com:8000"
 
-echo "Starting..."
+conquered='"success":true'
+
+echo "Hello friend! Lets conquer!"
 for cc in $(cat "country-codes.txt");	
-	do 
-	printf "\nTrying $cc\n"
-	curl $URL -x USER:PASSWORD-country-$cc@la.residential.rayobyte.com:8000	
+	do 		
+	response=$(curl -s $URL --proxy $PROXY)
+	echo $response
+		
+	if [[ $response == *"$conquered"* ]]; then
+		printf "\nSuccess, $cc conquered!"		
+		echo $cc >> success-log.txt		
+	fi
 done
